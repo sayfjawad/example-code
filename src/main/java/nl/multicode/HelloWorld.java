ef7a9170694cc59979c495016d6d30cbc7927465
sayf <sayf.jawad@alliander.com>
2022-08-22 13:29:04 +0200

    Fixed build.gradle to create JAR

--- src/main/java/nl/multicode/HelloWorld.java
+++ src/main/java/nl/multicode/HelloWorld.java
@@ -1,7 +1,8 @@
-package nl.multicode
+package nl.multicode;
+
 public class HelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
